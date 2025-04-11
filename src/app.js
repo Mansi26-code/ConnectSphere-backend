05812cac725076c47763974610bd5eb48b4a8c08
Mansi Swaraj <mansiswaraj26@gmail.com>
2025-04-11 22:07:38 +0530

    message added

--- src/app.js
+++ src/app.js
@@ -25,6 +25,9 @@ app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/api/v1/users", userRoutes);
+app.get("/", (req, res) => {
+    res.send("Server is working fine");
+  });
 
 const start = async () => {
     try {
